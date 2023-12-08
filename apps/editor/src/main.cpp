02f58d964b7a7fa7f0f80adf98eafc9154be6249
Max Franz <maex.payn@googlemail.com>
2023-12-08 13:11:52 +0100

    added build and install data path handling

--- apps/editor/src/main.cpp
+++ apps/editor/src/main.cpp
@@ -1,6 +1,7 @@
 // rurouni
 #include "rurouni/dev/logger.hpp"
 #include "rurouni/editor/logger.hpp"
+#include "rurouni/editor/config.h"
 
 // std
 #include <memory>
@@ -13,7 +14,13 @@ int main() {
     rr::dev::Logger::init(loggerSpec);
 
     rr::runtime::LOG = std::make_unique<rr::dev::Logger>("editor");
-    rr::runtime::LOG->info("Hello from editor!");
+    rr::runtime::LOG->info("bin dir ......... : {}", RR_EDITOR_BIN_DIR);
+    rr::runtime::LOG->info("lib dir ......... : {}", RR_EDITOR_LIB_DIR);
+    rr::runtime::LOG->info("archive dir ..... : {}", RR_EDITOR_ARCHIVE_DIR);
+    rr::runtime::LOG->info("shared data dir . : {}", RR_EDITOR_SHARED_DATA_DIR);
+    rr::runtime::LOG->info("shared config dir : {}", RR_EDITOR_SHARED_CONFIG_DIR);
+    rr::runtime::LOG->info("user data dir ... : {}", RR_EDITOR_USER_DATA_DIR);
+    rr::runtime::LOG->info("user config dir . : {}", RR_EDITOR_USER_CONFIG_DIR);
 
     return 0;
 }
--- apps/runtime/src/main.cpp
+++ apps/runtime/src/main.cpp
@@ -1,5 +1,9 @@
+// rurouni
 #include "rurouni/dev/logger.hpp"
 #include "rurouni/runtime/logger.hpp"
+#include "rurouni/runtime/config.h"
+
+// std
 #include <memory>
 
 int main() {
@@ -10,7 +14,13 @@ int main() {
     rr::dev::Logger::init(loggerSpec);
 
     rr::runtime::LOG = std::make_unique<rr::dev::Logger>("runtime");
-    rr::runtime::LOG->info("Hello from runtime!");
+    rr::runtime::LOG->info("bin dir ......... : {}", RR_RUNTIME_BIN_DIR);
+    rr::runtime::LOG->info("lib dir ......... : {}", RR_RUNTIME_LIB_DIR);
+    rr::runtime::LOG->info("archive dir ..... : {}", RR_RUNTIME_ARCHIVE_DIR);
+    rr::runtime::LOG->info("shared data dir . : {}", RR_RUNTIME_SHARED_DATA_DIR);
+    rr::runtime::LOG->info("shared config dir : {}", RR_RUNTIME_SHARED_CONFIG_DIR);
+    rr::runtime::LOG->info("user data dir ... : {}", RR_RUNTIME_USER_DATA_DIR);
+    rr::runtime::LOG->info("user config dir . : {}", RR_RUNTIME_USER_CONFIG_DIR);
 
     return 0;
 }
